7e249c1504603004ae526a54e0d32dec36c1d4cb
John Wells <molekewl2017@gmail.com>
2022-01-30 20:29:46 -0800

    css / banner component

---
+++ components/banner/banner.js
@@ -0,0 +1,43 @@
+import Image from 'next/image';
+import styles from './banner.module.css';
+
+const Banner = ({ title, subTitle, imgUrl }) => {
+  const handleOnPlay = () => {
+    console.log('Play');
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.leftWrapper}>
+        <div className={styles.left}>
+          <div className={styles.nseriesWrapper}>
+            <p className={styles.firstLetter}>N</p>
+            <p className={styles.series}>S E R I E S</p>
+          </div>
+          <h1 className={styles.title}>{title}</h1>
+          <p className={styles.subTitle}>{subTitle}</p>
+
+          <div className={styles.playBtnWrapper}>
+            <button className={styles.btnWithIcon} onClick={handleOnPlay}>
+              <Image
+                src='/static/play_arrow.svg'
+                alt='play icon'
+                width='32px'
+                height='32px'
+              />
+              <span className={styles.playText}>Play</span>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className={styles.bannerImg}
+        style={{
+          backgroundImage: `url(${imgUrl})`,
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default Banner;
