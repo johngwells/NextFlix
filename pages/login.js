036e1724dbf4413e29187779e01efa80d94541f2
John Wells <molekewl2017@gmail.com>
2022-01-31 21:46:55 -0800

    login page with css / error validation

--- pages/login.js
+++ pages/login.js
@@ -1,21 +1,66 @@
+import { useState } from 'react';
+import Router, { useRouter } from 'next/router';
 import Head from 'next/head';
+
 import styles from '../styles/Login.module.css';
 
 const Login = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState('');
+  const [userMsg, setUserMsg] = useState('');
+
+  const handleOnChangeEmail = e => {
+    setUserMsg('');
+    const email = e.target.value;
+    setEmail(email);
+  };
+
+  const handleLoginWithEmail = e => {
+    e.preventDefault();
+    if (email) {
+      if (email === 'johnwells@google.com') {
+        // route to dashboard
+        router.push('/');
+        setUserMsg('verified! routing to dashboard...')
+      } else {
+        // show error
+        setUserMsg('You don\'t have login access');
+      }
+    } else {
+      setUserMsg('Enter a valid email address');
+    }
+  };
+
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Nextflix SignIn</title>
       </Head>
-      <div className={styles.container}>
-        <div className={styles.header}>
-          <div className={styles.headerWrapper}>
-            <a className={styles.logoLink} href='/'>
-              <div className={styles.logoWrapper}>NEXTFLIX</div>
-            </a>
-          </div>
+      <header className={styles.header}>
+        <div className={styles.headerWrapper}>
+          <a className={styles.logoLink} href='/'>
+            <div className={styles.logoWrapper}>NEXTFLIX</div>
+          </a>
         </div>
-      </div>
+      </header>
+
+      <main className={styles.main}>
+        <div className={styles.mainWrapper}>
+          <h1 className={styles.signinHeader}>Sign in</h1>
+
+          <input
+            type='text'
+            placeholder='Email address'
+            className={styles.emailInput}
+            onChange={handleOnChangeEmail}
+          />
+
+          <p className={styles.userMsg}>{userMsg}</p>
+          <button onClick={handleLoginWithEmail} className={styles.loginBtn}>
+            Sign in
+          </button>
+        </div>
+      </main>
     </div>
   );
 };
