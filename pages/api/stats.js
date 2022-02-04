755830d66ebfd8dd45879883e77be9a1d1b710c2
John Wells <molekewl2017@gmail.com>
2022-02-03 23:42:10 -0800

    remove hard coded data & accept from body/response. Also add validation for missing inputs

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -9,30 +9,34 @@ export default async function stats(req, res) {
       if (!token) {
         res.status(403).send({ msg: 'error' });
       } else {
-        const videoId = req.query.videoId;
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-        const userId = decodedToken.issuer;
-        console.log(req.query);
+        const { videoId, favorited, watched = true } = req.body;
+        if (videoId) {
+          const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+          const userId = decodedToken.issuer;
+          console.log(req.query);
 
-        const statsExist = await findVideoIdByUser(userId, videoId, token);
-        if (statsExist) {
-          // update it
-          const response = await updateStats(token, {
-            watched: false,
-            userId,
-            videoId: 'eeZ1Ufdra0E',
-            favorited: 0
-          });
-          res.send({ msg: 'it works', response });
+          const statsExist = await findVideoIdByUser(userId, videoId, token);
+          if (statsExist) {
+            // update it
+            const response = await updateStats(token, {
+              watched,
+              userId,
+              videoId,
+              favorited
+            });
+            res.send({ data: response });
+          } else {
+            // add it
+            const response = await insertStats(token, {
+              watched,
+              userId,
+              videoId,
+              favorited
+            });
+            res.send({ data: response });
+          }
         } else {
-          // add it
-          const response = await insertStats(token, {
-            watched: false,
-            userId,
-            videoId: 'Me50VkUdP2I',
-            favorited: 0
-          });
-          res.send({ msg: 'it works', response });
+          res.status(400).send({ error: 'can not update or insert, videoId is missing' });
         }
       }
     } catch (err) {
