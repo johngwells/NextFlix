7d31f29abd6aaea5a78b8ec3322868a43246c3a9
John Wells <molekewl2017@gmail.com>
2022-02-05 04:34:23 -0800

    user redirect to login if not valid in serverSideProps

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,16 @@ import useRedirectUser from '../utils/redirectUser';
 export async function getServerSideProps(context) {
   const { userId, token } = await useRedirectUser(context);
 
+  if (!userId) {
+    return {
+      props: {},
+      redirect: {
+        destination: '/login',
+        permanent: false
+      }
+    };
+  }
+
   const disneyVideos = await getVideos('disney trailer');
   const animeVideos = await getVideos('anime trailer');
   const popularVideos = await getPopularVideos();
--- utils/redirectUser.js
+++ utils/redirectUser.js
@@ -3,15 +3,7 @@ import { verifyToken } from '../lib/utils';
 const useRedirectUser = async context => {
   const token = context.req ? context.req?.cookies.token : null;
   const userId = await verifyToken(token);
-  if (!userId) {
-    return {
-      props: {},
-      redirect: {
-        destination: '/login',
-        permanent: false
-      }
-    };
-  }
+
   return {
     userId,
     token
